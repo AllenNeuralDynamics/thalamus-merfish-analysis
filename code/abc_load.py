d9e039c41d56b70ae1156a3730acce3a349e6aa9
tmchartrand <tom.chartrand@alleninstitute.org>
2023-10-02 17:54:50 +0000

    Update ABC load code for Sep release

--- code/abc_load.py
+++ code/abc_load.py
@@ -14,29 +14,21 @@ _CIRRO_COLUMNS = {
     'parcellation_substructure':'CCF_acronym'
 }
 
+# older data asset subsetted to thalamus/ZI outline
 _REFERENCE_ANNDATA_PATH = "/data/merfish_638850_AIT17.custom_CCF_annotated_TH_ZI_only_2023-05-04_00-00-00/atlas_brain_638850_AIT17_custom_CCF_annotated_TH_ZI_only.h5ad"
 
 abc_root = Path("/data/abc_atlas/")
 
-with open(abc_root/"releases/20230630/manifest.json") as file:
-    manifest = json.load(file)
-
-def _md_dict_merfish():
-    return manifest['file_listing']['MERFISH-C57BL6J-638850']['metadata']
-
-def _md_dict_ccf():
-    return manifest['file_listing']['Allen-CCF-2020']['metadata']
-
-def get_combined_metadata(cirro_names=True, flip_y=True):
+def get_combined_metadata(cirro_names=True, flip_y=True, version="20230830"):
     # this info is redundant but includes more cells (QC'ed out of CCF process?) 
     # cells_df = pd.read_csv(
-    #     abc_root/"metadata/MERFISH-C57BL6J-638850/20230630/views/cell_metadata_with_cluster_annotation.csv",
+    #     abc_root/f"metadata/MERFISH-C57BL6J-638850/{version}/views/cell_metadata_with_cluster_annotation.csv",
     #                        dtype={'cell_label':str}, index_col=0)
     # cells_df.rename(columns={'x':'x_section','y':'y_section','z':'z_section'}, inplace=True)
 
     # could save space by not merging color etc
     ccf_df = pd.read_csv(
-        abc_root/"metadata/MERFISH-C57BL6J-638850-CCF/20230630/views/cell_metadata_with_parcellation_annotation.csv", 
+        abc_root/f"metadata/MERFISH-C57BL6J-638850-CCF/{version}/views/cell_metadata_with_parcellation_annotation.csv", 
                          dtype={'cell_label':str}, index_col=0)
     if flip_y:
         ccf_df[['y_section', 'y_reconstructed']] *= -1
@@ -49,12 +41,13 @@ def get_combined_metadata(cirro_names=True, flip_y=True):
     return cells_df
 
 
-def load_adata(with_metadata=True, transform='log2', cirro_names=True, subset_to_ref=True):
-    adata = ad.read_h5ad(abc_root/f"expression_matrices/MERFISH-C57BL6J-638850/20230630/C57BL6J-638850-{transform}.h5ad", backed='r')
+def load_adata(with_metadata=True, transform='log2', cirro_names=False, version="20230830", subset_to_ref=True):
+    adata = ad.read_h5ad(abc_root/f"expression_matrices/MERFISH-C57BL6J-638850/{version}/C57BL6J-638850-{transform}.h5ad", backed='r')
     if subset_to_ref:
         adata_ref = ad.read_h5ad(_REFERENCE_ANNDATA_PATH, backed='r')
         adata = adata[adata.obs_names.intersection(adata_ref.obs_names)]
     adata = adata.to_memory()
+    # access genes by short symbol vs longer names
     adata.var_names = adata.var['gene_symbol']
     if with_metadata:
         cells_df = get_combined_metadata(cirro_names=cirro_names)
