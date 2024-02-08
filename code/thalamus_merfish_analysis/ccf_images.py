b5a634bde2d2cffe8f0d0c6c1dd2d1fe9a3aee01
tmchartrand <tom.chartrand@alleninstitute.org>
2024-02-08 22:24:14 +0000

    ccf images function

--- code/thalamus_merfish_analysis/ccf_images.py
+++ code/thalamus_merfish_analysis/ccf_images.py
@@ -41,3 +41,17 @@ def label_erosion(label_img, distance_px, fill_val=0, return_edges=False):
                           np.where(result_img==0, fill_val, 
                                    result_img))
     return result_img
+
+
+def map_label_values(label_img, mapping, section_list=None):
+    if section_list is None:
+        section_list = range(label_img.shape[2])
+    subset = label_img[:,:,section_list]
+    label_vals = np.unique(subset)
+    result_img = np.zeros_like(subset)
+    for i in label_vals:
+        mask = (label_img[:,:,section_list]==i)
+        result_img += mapping[i]*mask
+    full_result = np.zeros_like(label_img)
+    full_result[:,:,section_list] = result_img
+    return full_result
