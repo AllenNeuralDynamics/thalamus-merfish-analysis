ba0d18b6c6fc8c610b9b6f6963c0459282975c7b
meghanaturner <meghan.turner@alleninstitute.org>
2024-04-24 23:27:01 +0000

    completed abc_to_cirro.py module and the corresponding 7_export_thalamus_data_for_cirrocumulus demo notebook

--- code/thalamus_merfish_analysis/abc_to_cirro.py
+++ code/thalamus_merfish_analysis/abc_to_cirro.py
@@ -1,51 +1,74 @@
-import numpy as np
 import pandas as pd
 import anndata as ad
 import scanpy as sc
+import seaborn as sns
+import colorcet as cc
 
-from .abc import (load_adata_thalamus, 
-                  filter_by_class_thalamus,
-                  filter_by_thalamus_coords,
-                  get_taxonomy_palette)
+from .abc_load import (load_adata_thalamus, 
+                       filter_by_class_thalamus,
+                       filter_by_thalamus_coords,
+                       get_taxonomy_palette)
 
-def make_h5ad_for_cirro():
-    ''' Load ABC Atlas data, filter by thalamus, and save as h5ad for cirrocumulus.
+def export_h5ad_for_cirro():
+    ''' Load thalamus subset of ABC Atlas, modify for Cirrocumulus compatibility,
+    and save out as h5ad file.
+
+    Returns
+    -------
+    adata : Cirrocumulus-compatible AnnData object
+    /results/abc_atlas_mouse_thalamus_cirro.h5ad : h5ad file containing adata
     '''
+    print('Loading thalamus subset from ABC Atlas ...')
     adata = load_adata_for_cirro()
+
+    print('Generating spatial coordinates for Cirrocumulus...')
     adata = add_montage_coords(adata)
     adata = add_coords_to_obsm(adata)
+
+    print('Adding color palettes for Cirrocumulus...')
     adata = add_colors_to_uns(adata)
+
+    print('Calculating low-dimensional embeddings ... This may take 10-15 minutes.')
     adata = add_umap_tsne_to_obsm(adata)
 
-    # TODO: add spagcn domains and nsf patterns to adata.obs
-    # adata = add_spagcn_to_obs(adata)
-    # adata = add_nsf_to_obs(adata)
+    # add unsupervised machine learning results
+    print('Adding unsupervised machine learning results')
+    adata = add_spagcn_to_adata(adata)
+    adata = add_nsf_to_obs(adata)
 
+    # save as h5ad
     adata.write_h5ad('/results/abc_atlas_mouse_thalamus_cirro.h5ad',
                      compression="gzip")
 
+    return adata
+
 
 def load_adata_for_cirro():
+    ''' Load thalamus subset of ABC Atlas as AnnData object.
+
+    Cannot use `load_standard_thalamus()` because y coords need to be inverted  
+    to display the brain sections in their proper orientation in Cirrocumulus.
+    '''
     # load with inverted y coord so sections appear in correct coronal orientation
-    adata = abc.load_adata_thalamus(flip_y=True)
+    adata = load_adata_thalamus(flip_y=True)
 
     # filter by class
-    adata = abc.filter_by_class_thalamus(adata,
+    adata = filter_by_class_thalamus(adata,
                                          filter_nonneuronal=True,
                                          filter_midbrain=False,
                                          filter_other_nonTH=True)
     
     # filter by thalamus coordinates
-    adata = abc.filter_by_thalamus_coords(adata, 
+    adata = filter_by_thalamus_coords(adata, 
                                           buffer=0)
 
     return adata
 
 
 def add_montage_coords(adata, 
-                       section_col='z_section',
-                       x_col='x_section',
-                       y_col='y_section',
+                       section_col='z_reconstructed',
+                       x_col='x_reconstructed',
+                       y_col='y_reconstructed',
                        new_coord_suffix='_cirro',
                        n_cols=4):
     ''' Creates 2D montage spatial coordinates from 2D+section coordinates and 
@@ -72,19 +95,20 @@ def add_montage_coords(adata,
     adata : AnnData object
         with x & y montage spatial coordinates added to adata.obs
     '''
+    obs = adata.obs.copy()
 
-    # copy over original xy coords                 
-    adata.obs['x'+new_coord_suffix] = adata.obs[x_col].copy()
-    adata.obs['y'+new_coord_suffix] = adata.obs[y_col].copy()
+    # copy over original xy coords to new columns             
+    obs['x'+new_coord_suffix] = obs[x_col]
+    obs['y'+new_coord_suffix] = obs[y_col]
 
     # Dynamically set x_shift, y_shift based on current x_col, y_col units
-    width_max = abs(adata.obs[x_col].max() - adata.obs[x_col].min())
-    height_max = abs(adata.obs[y_col].max() - adata.obs[y_col].min())
+    width_max = abs(obs[x_col].max() - obs[x_col].min())
+    height_max = abs(obs[y_col].max() - obs[y_col].min())
     # set x_shift and y_shift as a ratio of max width/height
     x_shift = width_max*1.2
     y_shift = -(height_max*1.5) # neg to start each new row below previous row
 
-    sections = sorted(adata.obs[section_col].unique())
+    sections = sorted(obs[section_col].unique())
     count = 0
     # start with anterior-most section in top left and end with posterior-most 
     # section in bottom right
@@ -95,11 +119,13 @@ def add_montage_coords(adata,
         curr_y_shift = y_shift * (i // n_cols)
         
         # apply x,y shifts to current section
-        mask = adata.obs[section_col]==sec
-        adata.obs.loc[mask, 'x'+new_coord_suffix] += curr_x_shift
-        adata.obs.loc[mask, 'y'+new_coord_suffix] += curr_y_shift
+        mask = obs[section_col]==sec
+        obs.loc[mask, 'x'+new_coord_suffix] += curr_x_shift
+        obs.loc[mask, 'y'+new_coord_suffix] += curr_y_shift
         
         count+=1
+
+    adata.obs = obs
     
     return adata
 
@@ -153,7 +179,7 @@ def add_colors_to_uns(adata):
 
     for level in taxonomy_levels:
         # get the full ABC color palette for this taxonomy level
-        abc_color_dict = abc.get_taxonomy_palette(level)
+        abc_color_dict = get_taxonomy_palette(level)
 
         # get the categories that exist in this dataset
         # MUST be kept in the order returned by .cat.categories
@@ -181,11 +207,11 @@ def add_umap_tsne_to_obsm(adata):
     sc.pp.neighbors(adata)
 
     # generate UMAP (saved to adata.obsm['X_umap'])
-    display('Generating UMAP embedding. This may take 1-2 minutes ...')
+    print('Generating UMAP embedding. This may take 1-2 minutes ...')
     sc.tl.umap(adata)
 
     # calculate tSNE (saved to adata.obsm['X_tsne'])
-    display('Generating tSNE embedding. This may take approx. 10 minutes ...')
+    print('Generating tSNE embedding. This may take approx. 10 minutes ...')
     sc.tl.tsne(adata)
 
     # cleanup unneeded data generated by pca, neighbors, tsne
@@ -198,36 +224,71 @@ def add_umap_tsne_to_obsm(adata):
 
     return adata
 
-def add_spagcn_to_adata(adata, domains_to_add='res1pt4', spagcn_col='SpaGCN_domains'):
+def add_spagcn_to_adata(adata, domains_to_add='res1pt4', spagcn_col='spagcn_domains'):
     ''' Add result domains from SpaGCN package to adata.obs & custom color 
     palette to adata.uns.
+
+    Parameters
+    ----------
+    adata : AnnData object
+    domains_to_add : str
+        column name in spagcn_df with SpaGCN domains (just one for now)
+    spagcn_col : str
+        name of new column to be added to adata.obs
+
+    Returns
+    -------
+    adata: with spagcn domains in adata.obs and corresponding colors in adata.uns
     '''
 
     # load in SpaGCN domain results
     # temporarily a static file in '../code/resources' until I get a reproducible run setup for the spagcn capsule
     spagcn_df = pd.read_parquet('/code/resources/spagcn_predicated_domains.parquet')
 
-    # convert to categories, will keep in numerical order
     for col in spagcn_df.columns:
-        spagcn_df[col] = pd.Categorical(spagcn_df[col], 
-                                        categories=sorted(spagcn_df[col].unique()))
+        # preserve numerical order when converting values to str, then column to categorical
+        cats_str = [str(x) for x in sorted(spagcn_df[col].unique())]
+        spagcn_df[col] = pd.Categorical(spagcn_df[col].astype(str), 
+                                        categories=cats_str)
 
-    # for now, just grab the first domain prediction
+    # for now, just grab one domain result
     spagcn_df.rename(columns={domains_to_add:spagcn_col}, inplace=True)
 
-    # add spagcn domains to adata.obs 
-    adata.obs = adata.obs.join(spagcn_df[spagcn_col], on='cell_label')
-    # fill NaN with new 'no data' category (added to end of categories list)
-    adata.obs[spagcn_col] = adata.obs[spagcn_col].cat.add_categories('no data').fillna('no data')
+    # add spagcn domains to copy of adata.obs
+    obs_spagcn = adata.obs.join(spagcn_df[spagcn_col], on='cell_label')
+
+    # fill NaNs from .join with new 'no data' category
+    obs_spagcn[spagcn_col] = obs_spagcn[spagcn_col].cat.add_categories('no data').fillna('no data')
 
     # generate color palette for spagcn domains
-    spagcn_cats = adata.obs[spagcn_col].cat.categories
+    spagcn_cats = obs_spagcn[spagcn_col].cat.categories
     spg_colors = sns.color_palette(cc.glasbey, n_colors=len(spagcn_cats)).as_hex()
     # set the 'no data' category color to white so it doesn't show up in cirro
     spg_colors[-1] = '#ffffff'
-
-    # combine cats & colors into dict & save to 
+    # combine cats & colors into dict
     spg_palette = dict(zip(spagcn_cats, spg_colors))
+    # add spagcn domain colors to adata.uns
     adata.uns[spagcn_col+'_color'] = spg_palette
+
+    # add updated obs back into adata
+    adata.obs = obs_spagcn
+
+    return adata
+
+
+def add_nsf_to_obs(adata):
+    '''Add all calculated NSF patterns to adata.obs. '''
+
+    # load NSF results
+    adata_nsf = ad.read_zarr("/root/capsule/data/nsf_2000_adata/nsf_2000_adata.zarr")
+
+    # get NSF pattern columns from obs
+    nsf_cols = adata_nsf.obs.columns.str.startswith('nsf')
+    nsf_patterns_df = adata_nsf.obs.loc[:, nsf_cols]
+    
+    # add NSF patterns to adata.obs
+    # done in two steps to avoid chained assignment errors
+    obs_with_nsf = adata.obs.join(nsf_patterns_df, on='cell_label')
+    adata.obs = obs_with_nsf
 
     return adata
