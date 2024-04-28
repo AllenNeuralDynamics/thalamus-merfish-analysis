700d1ac152455e9e9cc5afcaa5ee3c33f6e7f985
meghanaturner <meghan.turner@alleninstitute.org>
2024-04-28 20:19:03 +0000

    continued diversity metrics cleanup WIP

--- code/thalamus_merfish_analysis/diversity_metrics.py
+++ code/thalamus_merfish_analysis/diversity_metrics.py
@@ -1,12 +1,6 @@
 import numpy as np
 import pandas as pd
 
-TH_DIVERSITY_REGIONS = ['AD', 'AV', 'AM', 'CL', 'CM', 'IAD', 'IMD',
-                        'LD', 'LGd', 'LH', 'LP', 'MD', 'MH', 
-                        'PCN', 'PF', 'PO', 'PVT', 'RE', 'RT', 'SPA',
-                        'VAL', 'VM', 'VPL', 'VPM', 'VPMpc'
-                        ]
-
 
 def calculate_diversity_metrics(obs_ccf, 
                                 ccf_label='parcellation_structure_eroded'):
@@ -25,17 +19,32 @@ def calculate_diversity_metrics(obs_ccf,
         dataframe with diversity metrics for each region in the thalamus
     '''
 
-    # calculate a pre-selected set of diversity metrics for each region
+    # calculate a pre-selected set of diversity metrics for each CCF region
     th_metrics_df = pd.concat([
-        get_region_metric(obs_ccf, n_unique, "n", ccf_label=ccf_label),
-        get_region_metric(obs_ccf, n_unique, "frac", ccf_label=ccf_label, 
-                          norm_fcn=n_unique),
-        get_region_metric(obs_ccf, unique_norm, "n_norm", ccf_label=ccf_label),
+        # number of unique categories (e.g. clusters) found in each region
+        get_region_metric(obs_ccf, count_unique, "count", ccf_label=ccf_label),
+        # fraction of all possible categories (e.g. clusters) uniquely found in each region
+        get_region_metric(obs_ccf, count_unique, "frac", ccf_label=ccf_label, 
+                          norm_fcn=count_unique),
+        # number of unique categories found in each region, normalized by the 
+        # total number of cells present in that region
+        # controls for differences in cell density, region size
+        get_region_metric(obs_ccf, count_unique_norm, "count_norm2cells", ccf_label=ccf_label),
+        # number of unique categories with more than 5 cells
         get_region_metric(obs_ccf, count_gt5, "count_gt5", ccf_label=ccf_label),
-        get_region_metric(obs_ccf, inverse_simpsons, "inverse_simpsons", ccf_label=ccf_label),
+        # fraction of all possible categories (e.g. clusters) with >5 cells found in each region
+        get_region_metric(obs_ccf, count_gt5, "frac_gt5", ccf_label=ccf_label, 
+                          norm_fcn=count_unique),
+        # inverse simpsons diversity index
+        get_region_metric(obs_ccf, inverse_simpsons_index, "inverse_simpsons", ccf_label=ccf_label),
+        # shannon diversity index
+        get_region_metric(obs_ccf, shannon_index, "shannon_index", ccf_label),
+        # get_region_metric(obs_ccf, lambda x: 2**calc_shannon_index(x), "shannon_diversity", ccf_label),
+        # get_region_metric(obs_ccf, calc_shannon_index, "shannon_index_norm", ccf_label,
+        #                   norm_fcn=lambda x: np.log2(len(x.unique()))
         ], axis=1)
 
-    th_metrics_df['n_cell'] = obs_ccf[ccf_label].value_counts()
+    th_metrics_df['count_cells'] = obs_ccf[ccf_label].value_counts()
 
     return th_metrics_df
 
@@ -44,6 +53,7 @@ def get_region_metric(obs_ccf,
                       function, 
                       metric_name,
                       ccf_label='parcellation_structure_eroded',
+                      exclude=['unassigned','TH-unassigned'],
                       norm_fcn=None,
                       levels=['cluster','supertype','subclass']):
     ''' Calculate a metric for each region in the thalamus.
@@ -58,16 +68,16 @@ def get_region_metric(obs_ccf,
         name of the metric, used for the column name
     ccf_label : str, default='parcellation_structure_eroded'
         column name in obs_ccf where the CCF annotations can be found
+    exclude : list of str, default=['unassigned','TH-unassigned']
+        list of categories in ccf_label to exclude from the analysis
     norm_fcn : function, optional, default=None
         function to use to normalize the metric
     levels : 
         levels to group by, by default ['cluster','supertype','subclass']
 
     '''
-    ccf_to_exclude = ['unassigned','TH-unassigned']
-
-    # calculate metric for each (region, level) pair
-    th_metrics_df = (obs_ccf.loc[lambda df: ~df[ccf_label].isin(ccf_to_exclude)]  # filter out unassigned regions
+    # calculate metric per region, for each level
+    th_metrics_df = (obs_ccf.loc[lambda df: ~df[ccf_label].isin(exclude)]  # filter out unassigned regions
                         .groupby(ccf_label, observed=True)[levels]  # groupby ccf_label col, keep only levels columns
                         .aggregate(function)  # apply function to each (region, level) pair
                         .rename(columns=lambda x: "_".join([metric_name, x])))  # rename columns to '[metric_name]_[level]'
@@ -79,20 +89,22 @@ def get_region_metric(obs_ccf,
     return th_metrics_df
 
 
-def n_unique(x):
-    ''' Count the number of unique elements in a series.'''
+def count_unique(x):
+    ''' Count the number of unique elements in x (a pd.Series 
+    or 1D np.array).
+    '''
     return len(x.unique())
 
-def unique_norm(x):
-    ''' Count the number of unique elements in a series, normalized by the 
-    total number of elements.'''
+def count_unique_norm(x):
+    ''' Count the number of unique elements in x (a pd.Series 
+    or 1D np.array), normalized by the total number of items.'''
     return len(x.unique())/len(x)
 
 def count_gt5(x):
     '''Count the number of unique elements in a series that occur more than 5 times.'''
     return len(x.value_counts().loc[lambda c: c>5])
 
-def inverse_simpsons(x):
+def inverse_simpsons_index(x):
     '''Calculate the Inverse Simpson's Index for a series.
     
     The Inverse Simpson's Index (ISI) ranges from 1 to inf, where 1 
@@ -100,17 +112,19 @@ def inverse_simpsons(x):
     '''
     return 1/np.sum((x.value_counts().loc[lambda c: c>0]/len(x))**2)
 
-
-def calc_shannon_index(obs_col):
+def shannon_index(x):
+    '''Calculate the Shannon Diversity Index for a pandas Series.
+    
+    Shannon diversity index, normalized to (0,1)
+    1 = high diversity (clusters found in equal proportions)
+    0 = low diversity (some clusters found in higher proportions in subregion)
+    '''
     # calculate proportion of each category
-    cateogory_counts = obs_col.value_counts()
+    cateogory_counts = x.value_counts()
     cateogory_counts = cateogory_counts[cateogory_counts>0] # cleanup zeros so log() doesn't throw warnings
     p = cateogory_counts / cateogory_counts.sum()
     
     # calculate shannon diversity index
     shannon_ind = (-1)*((p * np.log2(p)).sum())
     
-    # normalized separately
-    # normalize by log2 of number of categories to limit index to range (0, 1)
-    # shannon_norm = shannon_ind / np.log2(n_total_categories)
     return shannon_ind
--- code/thalamus_merfish_analysis/diversity_plots.py
+++ code/thalamus_merfish_analysis/diversity_plots.py
@@ -1,43 +1,63 @@
-def barplot_counts_fractions(th_metrics, level, thresholded=True):
+import matplotlib.pyplot as plt
+
+
+TH_DIVERSITY_REGIONS = ['AD', 'AV', 'AM', 'CL', 'CM', 'IAD', 'IMD',
+                        'LD', 'LGd', 'LH', 'LP', 'MD', 'MH', 
+                        'PCN', 'PF', 'PO', 'PVT', 'RE', 'RT', 'SPA',
+                        'VAL', 'VM', 'VPL', 'VPM', 'VPMpc'
+                        ]
+
+
+def barplot_dual_y_count_frac(th_metrics, level, gt5_only=True):
+    ''' Plot a barplot with both count and fraction of cells in each thalamic region.
+
+    Parameters
+    ----------
+    th_metrics : pd.DataFrame
+        DataFrame made with diversity_metrics.calculate_diversity_metrics()
+    level : str, {'cluster', 'supertype', 'subclass'}
+        ABC Atlas taxonomy level to plot
+    gt5_only : bool
+        If True, use the _gt5 columns for count and fraction
+    '''
+    # set th_metrics col based on thresholding to >5 cells or not
+    count_col = f'count_gt5_{level}' if gt5_only else f'count_{level}'
+    frac_col = f'frac_gt5_{level}' if gt5_only else f'frac_{level}'
+    
+    # sort regions so they're displayed low to high count, L to R
+    th_metrics_sorted = th_metrics.sort_values(by=count_col, ascending=True)
+    
     fig, ax1 = plt.subplots(figsize=(8,4))
-    if thresholded:
-        count = f'count_gt5_{level}'
-        ntot = obs_neurons_ccf[level].value_counts().loc[lambda x: x>5].shape[0]
-    else:
-        count = f'n_{level}'
-        ntot = obs_neurons_ccf[level].value_counts().loc[lambda x: x>0].shape[0]
-    th_metrics_sorted = th_metrics.sort_values(by=count, ascending=True)
-    # Plot the absolute values on the left y-axis
-    ax1.scatter(th_metrics_sorted.index, th_metrics_sorted[count], color='#5DA7E5', alpha=0)
+    # Plot the absolute counts on the left y-axis
+    ax1.scatter(th_metrics_sorted.index, th_metrics_sorted[count_col], 
+                color='#5DA7E5', alpha=0)
     ax1.set_ylabel(f'unique {level} count', color='k')
     ax1.tick_params(axis='y', labelcolor='k')
     ax1.set_xticks(th_metrics_sorted.index)
     ax1.set_xticklabels(th_metrics_sorted.index, rotation=90)
-    ax1.set_ylim(0, th_metrics_sorted[count].max()*1.05)
+    ax1.set_xlabel('CCF thalamic subregions')
+    ax1.set_ylim(0, th_metrics_sorted[count_col].max()*1.05)
     plt.grid(visible=True, axis='y')
 
-    # Create a secondary y-axis on the right
-    ax2 = ax1.twinx()
-
     # Plot the fraction values on the right y-axis
-    frac = th_metrics_sorted[count].astype(float)/ntot
-    ax2.bar(th_metrics_sorted.index, frac, color='#5DA7E5', label=level)
-    ax2.set_ylabel(f'fraction of total count (n={ntot})', color='k', rotation=270, labelpad=15)
-    ax2.set_ylim(0, frac.max()*1.05)
+    ax2 = ax1.twinx()
+    ax2.bar(th_metrics_sorted.index, th_metrics_sorted[frac_col], 
+            color='#5DA7E5', label=level)
+    # ntot = obs_neurons_ccf[level].value_counts().loc[lambda x: x>5].shape[0]
+    ax2.set_ylabel(f'fraction of total {level} count', color='k', rotation=270, labelpad=15)
+    ax2.set_ylim(0, th_metrics_sorted[frac_col].max()*1.05)
     ax2.tick_params(axis='y', labelcolor='k')
-    # ax2.legend()
+    
 
-    # plt.xticks(rotation=90)
-    plt.xlabel('CCF thalamic subregions')
     plt.title(f'{level} count per thalamic CCF structure')
     return fig
 
-fig = barplot_counts_fractions(th_metrics.loc[regions_subset], 'cluster', thresholded=True)
-fig.savefig("/results/nuclei_cluster_counts_barplot.pdf", transparent=True)
-
-def plot_metrics_multiple_levels(th_metrics, metric, levels=['cluster','subclass']):
+def plot_metric_multiple_levels(th_metrics, 
+                                metric, 
+                                levels=['cluster','supertype','subclass']):
     fig, ax1 = plt.subplots(figsize=(8,4))
 
+    # sort by the metric of the first item in levels list
     th_metrics_sorted = th_metrics.sort_values(by="_".join([metric, levels[0]]), ascending=True)
     for level in levels[::-1]:
         ax1.scatter(th_metrics_sorted.index, th_metrics_sorted["_".join([metric, level])], 
@@ -47,28 +67,29 @@ def plot_metrics_multiple_levels(th_metrics, metric, levels=['cluster','subclass
     ax1.set_ylabel(metric)
     ax1.legend()
     plt.grid(visible=True, axis='both', zorder=0, color='whitesmoke')
-
-
-# display as a stacked bar graph
-from colorcet import glasbey_light
-
-def plot_stacked_barplot(df, taxonomy_level, legend=True, palette=None):
-    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(12,5))
-    if palette is None:
-        palette = abc.get_taxonomy_palette(taxonomy_level)
-        # palette = sns.color_palette(glasbey_light, n_colors=len(df.columns))
-    palette['other'] = 'grey'
-    df.plot(kind='bar', stacked=True, ax=axes, legend=legend, color=palette)
-    if legend:
-        axes.legend(loc='upper left', bbox_to_anchor=(0.05, -0.3), ncol=4)
-    axes.set_xticklabels(df.index, rotation=90)
-    axes.set_xlabel('CCF structure')
-    # axes.set_yticks([])
-    axes.set_ylabel('proportion of cells in unique '+taxonomy_level)
-
-    fig.subplots_adjust(hspace=0.1)
     
-    # add text
-    for i, subregion in enumerate(df.index):
-        n_nonzero = (df.loc[subregion, df.columns!='other']>0).sum()
-        axes.text(i, 1.01, n_nonzero, horizontalalignment='center')
+    return fig
+
+# # display as a stacked bar graph
+# from colorcet import glasbey_light
+
+# def plot_stacked_barplot(df, taxonomy_level, legend=True, palette=None):
+#     fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(12,5))
+#     if palette is None:
+#         palette = abc.get_taxonomy_palette(taxonomy_level)
+#         # palette = sns.color_palette(glasbey_light, n_colors=len(df.columns))
+#     palette['other'] = 'grey'
+#     df.plot(kind='bar', stacked=True, ax=axes, legend=legend, color=palette)
+#     if legend:
+#         axes.legend(loc='upper left', bbox_to_anchor=(0.05, -0.3), ncol=4)
+#     axes.set_xticklabels(df.index, rotation=90)
+#     axes.set_xlabel('CCF structure')
+#     # axes.set_yticks([])
+#     axes.set_ylabel('proportion of cells in unique '+taxonomy_level)
+
+#     fig.subplots_adjust(hspace=0.1)
+    
+#     # add text
+#     for i, subregion in enumerate(df.index):
+#         n_nonzero = (df.loc[subregion, df.columns!='other']>0).sum()
+#         axes.text(i, 1.01, n_nonzero, horizontalalignment='center')
