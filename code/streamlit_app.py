1dd49918b38ee9a1183d97c8bfe8ccb3dfbac17b
tmchartrand <tom.chartrand@alleninstitute.org>
2024-05-07 22:27:51 +0000

    streamlit app demo version

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -7,15 +8,22 @@ from thalamus_merfish_analysis import abc_load as abc
 # from abc_load import _get_ccf_metadata
 # _get_ccf_metadata = st.cache_data(_get_ccf_metadata)
 
+
+# TODO: cache growing when shouldn't be? or just in debug?
+st.set_page_config(
+    page_title="Thalamus MERFISH explorer",
+    layout="wide"
+)
 version = "20230830"
-realigned = st.radio("CCF alignment", [False, True], index=0,
+section_col = 'z_section'
+ccf_level = 'substructure'
+lump_structures = False
+with st.expander("CCF alignment settings"):
+    realigned = st.radio("CCF alignment", [False, True], index=0,
                      format_func = lambda realigned: 
                       'thalamus-specific section-wise affine alignment' if realigned 
                       else 'published nonlinear alignment'
                       )
-section_col = 'z_section'
-ccf_level = 'substructure'
-lump_structures = False
 if realigned:
     ccf_label = f'parcellation_{ccf_level}_realigned'
     coords = 'section'
@@ -26,12 +34,18 @@ else:
 th_names = [x for x in abc.get_thalamus_names() if not 'unassigned' in x]
 th_subregion_names = [x for x in abc.get_thalamus_names(level=ccf_level) if not 'unassigned' in x]
 palettes = {level: abc.get_taxonomy_palette(level) for level in 
-            ['subclass','supertype','cluster']}
+            ['subclass','supertype']}
+palette['cluster'] = abc.get_cluster_palette()
 cplots.CCF_REGIONS_DEFAULT = th_subregion_names
+
+has_realigned_asset = Path("/data/realigned/abc_realigned_metadata_thalamus-boundingbox.parquet").exists()
+if realigned and not has_realigned_asset:
+    realigned = False
+    st.warning("Realigned metadata not found, using published alignment")
 
 @st.cache_data
 def get_data(version, ccf_label):
-    obs = abc.get_combined_metadata(realigned=realigned, version=version, drop_unused=False)
+    obs = abc.get_combined_metadata(realigned=has_realigned_asset, version=version, drop_unused=False)
     # remove non-neuronal and some other outlier non-thalamus types
     obs_neurons = abc.filter_by_class_thalamus(obs, filter_midbrain=False)
     obs_th = obs[obs[ccf_label].isin(th_names)]
@@ -42,7 +56,7 @@ def get_data(version, ccf_label):
 
 
 @st.cache_data
-def get_image_volumes(realigned, sections_all, lump_structures=True, edge_width=2):
+def get_image_volumes(realigned, sections_all, lump_structures=True, edge_width=1):
     section_list = np.rint(np.array(sections_all)/0.2).astype(int)
     ccf_images = abc.get_ccf_labels_image(resampled=True, realigned=realigned)
     
@@ -59,123 +73,149 @@ def get_image_volumes(realigned, sections_all, lump_structures=True, edge_width=
     return ccf_images, ccf_boundaries
 
 obs_th_neurons, sections_all, subclasses_all = get_data(version, ccf_label)
-ccf_images, ccf_boundaries = get_image_volumes(realigned, sections_all, lump_structures=False)
+ccf_polygons, ccf_boundaries = get_image_volumes(realigned, sections_all, lump_structures=lump_structures)
 
-kwargs = dict(
-    bg_cells=obs_th_neurons,
+@st.cache_resource
+def get_adata(transform="cpm"):
+    return abc.load_adata(version=version, transform=transform, from_metadata=obs_th_neurons)
+
+pane1, pane2 = st.columns(2)
+common_args = dict(
     section_col=section_col,
     x_col = 'x_'+coords,
     y_col = 'y_'+coords,
-    s=3, 
-    face_palette=None,
-    edge_color='grey',
     boundary_img=ccf_boundaries
 )
+def full_section_name(section_z):
+    return f"Section {np.rint(section_z/200e-3)} (z={section_z})"
 
-
-# @st.cache_resource
-def plot(obs, sections, regions=None, point_hue='subclass'):
-    return cplots.plot_ccf_overlay(obs, ccf_images, 
-                                   ccf_names=regions,
-                                    point_hue=celltype_label, 
-                                    sections=sections,
-                                    point_palette=palettes[celltype_label],
-                                    **kwargs)
-
-tab1, tab2 = st.tabs([
-    "by thalamic nucleus",
-    "by section",
-])
-with tab2:
-    sections = st.multiselect(
-        "Section z coordinate", sections_all, 
-        default=[7.2]
-    )
-    celltype_label = st.selectbox(
-        "Level of celltype hierarcy",
-        ['subclass','supertype','cluster'], index=0,
-    )
-    show_legend = st.checkbox(
-        "Show legend"
-    )
-    if celltype_label=='cluster':
-        palette = None
-    else:
-        palette = palettes[celltype_label]
-    obs = obs_th_neurons.loc[lambda df: df['subclass'].isin(subclasses_all)]
-    if len(sections)>0 and len(obs)>0:
-        plots = cplots.plot_ccf_overlay(obs, ccf_images, 
-                                    ccf_names=None,
-                                        ccf_level=ccf_level,
-                                        point_hue=celltype_label, 
-                                        sections=sections,
-                                        point_palette=palette,
-                                        legend='cells' if show_legend else False,
-                                        **kwargs)
+with pane2:
+    st.header("Gene expression")
+    transform = st.radio("Gene expression units", ['log2cpm', 'log2cpv', 'raw'], index=0)
+    # TODO: wait until gene selection to load
+    adata = get_adata(transform=transform)
+    gene = st.selectbox("Select gene", adata.var_names, index=None)
+    nuclei = st.multiselect("Nuclei to highlight", th_subregion_names)
+    sections = st.multiselect("Sections", sections_all, format_func=full_section_name)
+    if len(sections)==0: sections = None
+    focus_plot = st.checkbox("Focus on selected nuclei") and len(nuclei)>0
+    if gene is not None:
+        plots = cplots.plot_expression_ccf(
+            adata, gene, ccf_polygons,
+            nuclei=cplots.CCF_REGIONS_DEFAULT, 
+            highlight=nuclei,
+            sections=sections,
+            zoom_to_highlighted=focus_plot,
+            **common_args
+        )
         for plot in plots:
             st.pyplot(plot)
 
+with pane1:
+    st.header("Cell type taxonomy annotations")
 
-nucleus_groups = {
-    "Motor": ['VAL','VM'],
-    "Vision": ['LGd','LP'],
-    "Somatosensory": ['VPM','VPL','PO'],
-    "Limbic/Anterior": ['AD','AV','AM'],
-    "Auditory": ['MG']
-}
-with tab1:
-    manual_annotations = st.radio("Nucleus vs cluster annotations", [True, False], index=0,
-                     format_func = lambda manual_annotations: 
-                      'manual' if manual_annotations 
-                      else 'automated'
-                      )
-    groups = st.multiselect(
-        "Select nucleus groups", nucleus_groups.keys(),
-    )
-    with st.empty():
-        if len(groups)>0:
-            preselect = set.union(*[set(nucleus_groups[group]) for group in groups])
-            nuclei = st.multiselect(
-                "Select individual nuclei", th_names,
-                default=preselect
-            )
-        else:
-            nuclei = st.multiselect(
-                "Select individual nuclei", th_names,
-            )
-    
-    celltype_label = st.selectbox(
-        "Level of celltype hierarcy",
-        ['subclass','supertype','cluster'], index=2,
-        key=2
-    )
-    include_shared_clusters = st.checkbox(
-        "Include shared clusters"
+    kwargs = dict(
+        bg_cells=obs_th_neurons,
+        s=3, 
+        **common_args
     )
     
-    try:
-        if len(nuclei)>0:
-            obs2 = pd.concat([
-                abc.get_obs_from_annotated_clusters(nucleus, obs_th_neurons, 
-                                                    include_shared_clusters=include_shared_clusters,
-                                                    manual_annotations=manual_annotations)
-                for nucleus in nuclei
-            ])
-            if len(obs2)>0:
-                regions = [x for x in th_subregion_names if any((name in x and not 'pc' in x) or (name==x) 
-                                                                for name in nuclei)]
-                plots = cplots.plot_ccf_overlay(obs2, ccf_images, 
-                                            ccf_names=regions,
+    def plot(obs, sections, regions=None, point_hue='subclass'):
+        return cplots.plot_ccf_overlay(obs, ccf_images, 
+                                    ccf_names=regions,
+                                        point_hue=celltype_label, 
+                                        sections=sections,
+                                        point_palette=palettes[celltype_label],
+                                        **kwargs)
+    tab1, tab2 = st.tabs([
+        "by thalamic nucleus",
+        "by section",
+    ])
+    with tab2:
+        sections = st.multiselect(
+            "Section", sections_all, format_func=full_section_name,
+        )
+        celltype_label = st.selectbox(
+            "Level of celltype hierarcy",
+            ['subclass','supertype','cluster'], index=0,
+        )
+        show_legend = st.checkbox(
+            "Show legend"
+        )
+        obs = obs_th_neurons.loc[lambda df: df['subclass'].isin(subclasses_all)]
+        if len(sections)>0 and len(obs)>0:
+            plots = cplots.plot_ccf_overlay(obs, ccf_polygons, 
+                                        ccf_names=None,
                                             ccf_level=ccf_level,
-                                            # highlight=nuclei, TODO: fix highlight for raster plots
                                             point_hue=celltype_label, 
-                                            sections=None,
-                                            min_group_count=0,
-                                            point_palette=None if celltype_label=='cluster' else palettes[celltype_label],
+                                            sections=sections,
+                                            point_palette=palettes[celltype_label],
+                                            legend='cells' if show_legend else False,
                                             **kwargs)
-                for plot in plots:
-                    st.pyplot(plot)
+            for plot in plots:
+                st.pyplot(plot)
+
+
+    nucleus_groups = {
+        "Motor": ['VAL','VM'],
+        "Vision": ['LGd','LP'],
+        "Somatosensory": ['VPM','VPL','PO'],
+        "Limbic/Anterior": ['AD','AV','AM'],
+        "Auditory": ['MG']
+    }
+    with tab1:
+        manual_annotations = st.radio("Nucleus vs cluster annotations", [True, False], index=0,
+                        format_func = lambda manual_annotations: 
+                        'manual' if manual_annotations 
+                        else 'automated'
+                        )
+        groups = st.multiselect(
+            "Select nucleus groups", nucleus_groups.keys(),
+        )
+        with st.empty():
+            if len(groups)>0:
+                preselect = set.union(*[set(nucleus_groups[group]) for group in groups])
+                nuclei = st.multiselect(
+                    "Select individual nuclei", th_subregion_names,
+                    default=preselect
+                )
             else:
-                st.write("No annotations found for nuclei")
-    except UserWarning as exc:
-        str(exc)
+                nuclei = st.multiselect(
+                    "Select individual nuclei", th_subregion_names,
+                )
+        
+        celltype_label = st.selectbox(
+            "Level of celltype hierarcy",
+            ['subclass','supertype','cluster'], index=2,
+            key=2
+        )
+        include_shared_clusters = st.checkbox(
+            "Include shared clusters"
+        )
+        
+        try:
+            if len(nuclei)>0:
+                obs2 = pd.concat([
+                    abc.get_obs_from_annotated_clusters(nucleus, obs_th_neurons, 
+                                                        include_shared_clusters=include_shared_clusters,
+                                                        manual_annotations=manual_annotations)
+                    for nucleus in nuclei
+                ])
+                if len(obs2)>0:
+                    regions = [x for x in th_subregion_names if any((name in x and not 'pc' in x) or (name==x) 
+                                                                    for name in nuclei)]
+                    plots = cplots.plot_ccf_overlay(obs2, ccf_polygons, 
+                                                ccf_names=regions,
+                                                ccf_level=ccf_level,
+                                                # highlight=nuclei, TODO: fix highlight for raster plots
+                                                point_hue=celltype_label, 
+                                                sections=None,
+                                                min_group_count=0,
+                                                point_palette=None if celltype_label=='cluster' else palettes[celltype_label],
+                                                **kwargs)
+                    for plot in plots:
+                        st.pyplot(plot)
+                else:
+                    st.write("No annotations found for nuclei")
+        except UserWarning as exc:
+            str(exc)
