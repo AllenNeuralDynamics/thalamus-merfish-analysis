52ee6daae5fad6159133e0ffdc74ef620d07eaad
tmchartrand <tom.chartrand@alleninstitute.org>
2024-05-22 20:56:00 +0000

    remove accidentally commited files

--- environment/CO_new_capsule_init.sh
+++
@@ -1,6 +0,0 @@
-
-# move capsule-internal files outside to allow chezmoi to manage?
-mv ~/capsule/code/.vscode ~/.vscode
-ln -s ~/.vscode ~/capsule/code/.vscode
-# this may not be needed once all chezmoi managed files out of capsule
-chezmoi apply
